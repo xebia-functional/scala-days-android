cb78f4d90463425d5261467c5f921550b325ded6
Javi Pacheco <javielinux@gmail.com>
2015-02-17 13:49:34 +0100

    Bug fixed in orientation changed

--- src/main/scala/com/fortysevendeg/android/scaladays/ui/places/PlacesFragment.scala
+++ src/main/scala/com/fortysevendeg/android/scaladays/ui/places/PlacesFragment.scala
@@ -18,7 +18,7 @@ package com.fortysevendeg.android.scaladays.ui.places
 
 import android.content.Intent
 import android.net.Uri
-import android.os.Bundle
+import android.os.{Handler, Bundle}
 import android.support.v4.app.Fragment
 import com.fortysevendeg.android.scaladays.model.Venue
 import com.fortysevendeg.android.scaladays.modules.ComponentRegistryImpl
