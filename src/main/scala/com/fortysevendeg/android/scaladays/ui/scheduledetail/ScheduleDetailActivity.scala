f189074d569f98cd5934cc81e12fc34ed5867461
Javi Pacheco <javielinux@gmail.com>
2015-05-13 10:08:56 +0200

    Removing isFavoriteEvent field

--- src/main/scala/com/fortysevendeg/android/scaladays/ui/scheduledetail/ScheduleDetailActivity.scala
+++ src/main/scala/com/fortysevendeg/android/scaladays/ui/scheduledetail/ScheduleDetailActivity.scala
@@ -54,8 +54,6 @@ class ScheduleDetailActivity
 
   private var idEvent: Option[Int] = None
 
-  private var isFavoriteEvent: Boolean = false
-
   override def onCreate(savedInstanceState: Bundle) = {
     super.onCreate(savedInstanceState)
 
@@ -131,7 +129,6 @@ class ScheduleDetailActivity
   private def favoriteClick(eventTitle: String, name: String) = {
     favoriteChanged = true
     val isFavorite = preferenceServices.fetchBooleanPreference(PreferenceRequest[Boolean](name, false)).value
-    isFavoriteEvent = !isFavorite
     if (isFavorite) {
       analyticsServices.sendEvent(
         screenName = Some(analyticsScheduleDetailScreen),
